4d89d326a3f5cb818c93dfaeccb0c3513d95d19f
root <root@debiansurik.surik>
2023-11-29 12:29:55 -0500

    move and rename file to test lib

--- build/CMakeFiles/akbase-shared.dir/source/ak_tools.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/akbase-shared.dir/source/ak_tools.c.o: \
-  /home/suriksurik/libakrypt-0.x/source/ak_tools.c \
-  /home/suriksurik/libakrypt-0.x/build/libakrypt-base.h
--- build/CMakeFiles/akrypt-shared.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for akrypt-shared.
-# This may be replaced when dependencies are built.
