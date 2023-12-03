a648a171bbbe2e4e394a3aef0c876181c6b21d2d
root <root@debiansurik.surik>
2023-12-03 11:54:49 -0500

    added new cases

--- examples/tests/test-curves.c
+++ examples/tests/test-curves.c
@@ -79,16 +79,16 @@ void test_ak_wcurve_discriminant_is_ok() {
   struct wcurve paramSetA256_5 = {
     ak_mpzn256_size,
     4, /* cofactor */
-    { 0x0111111111181048LL }, /* a */
-    { 0xacd1216d5cc63966LL, }, /* b */
-    { 0xfffffffffffffd97LL, 0xffffffffffffffffLL }, /* p */
-    { 0x000000000005cf11LL, 0x0001111231231100LL }, /* r2 */
-    { 0x0000000000000000LL }, /* q */
-    { 0x0000000000000000LL }, /* r2q */
+    { 0x0111111111181048LL, 0x074e9bffffffffffLL, 0xffffffff123f111fLL}, /* a */
+    { 0xacd1216d5cc63966LL, 0x88092345712b2bbfLL, 0x328794231a037b15LL}, /* b */
+    { 0xfffffffffffffd97LL, 0xffffffffffffffffLL, 0xab12341bffffffffLL }, /* p */
+    { 0x000000000005cf11LL, 0x0001111231231100LL, 0xffffffffffffffffLL }, /* r2 */
+    { 0x0000000000000000LL, 0xffffffffffffffffLL, 0x45623123ffffffffLL }, /* q */
+    { 0x0000000000000000LL, 0xffffffffffffffffLL, 0xffffffffffffffffLL }, /* r2q */
     {
-      { 0x8b2582fe742daa28LL, 0x9a5e82c0dLL }, /* px */
-      { 0xaf268adb32322e5cLL, 0x895786c4bb46e956LL, 0x32879423ab1a0375LL }, /* py */
-      { 0x0000000000000001LL, 0x0111111111111111LL, 0x0000000000000000LL }  /* pz */
+    { 0x8b2582fe742daa28LL, 0x9a5e82aac0dfffffLL, 0xffffffffffffffffLL }, /* px */
+    { 0xaf268adb32322e5cLL, 0x895786c4bb46e956LL, 0x32879423ab1a0375LL }, /* py */
+    { 0x0000000000000001LL, 0x0111111111111111LL, 0x0000000000000000LL }  /* pz */
     },
     0x0111111111111111LL, /* n */
     0x0000000000000000LL, /* nq */
@@ -99,9 +99,9 @@ void test_ak_wcurve_discriminant_is_ok() {
     ak_mpzn256_size,
     4, /* cofactor */
     { 0x6d0078e62fc81048LL, 0x94db4f98bfb73698LL, 0x75e9b60631449efdLL, 0xca0709cc398e1cd1LL }, /* a */
-    { 0xacd1216d5cc63966LL, 0x534b728e6773c810LL, 0xfb4e95d31a5032feLL, 0xb76e3775f6a4aee7LL }, /* b */
-    { 0xfffffffffffffd97LL, 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xffffffffffffffffLL }, /* p */
-    { 0x000000000005cf11LL, 0x0000000000000000LL, 0x0000000000000000LL, 0x0000000000000000LL }, /* r2 */
+    { 0x0000001231230123LL, 0x0000012312300005LL, 0xfb4e95d31a5032feLL, 0xffffffffffffffffLL }, /* b */
+    { 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xffffffffffffffffLL }, /* p */
+    { 0x0000000012310123LL, 0x0001231231230204LL, 0x000d59a120000000LL, 0x0000000000000000LL }, /* r2 */
     { 0xc115af556c360c67LL, 0x0fd8cddfc87b6635LL, 0x0000000000000000LL, 0x4000000000000000LL }, /* q */
     { 0x57cb446240dd1710LL, 0x7556091c4805caa4LL, 0xd0593365f9384bcdLL, 0x0fb1fbc48b0f0eb4LL }, /* r2q */
     {
@@ -109,8 +109,8 @@ void test_ak_wcurve_discriminant_is_ok() {
       { 0xaf268adb32322e5cLL, 0x5fde0b5344766740LL, 0x895786c4bb46e956LL, 0x32879423ab1a0375LL }, /* py */
       { 0x0000000000000001LL, 0x0000000000000000LL, 0x0000000000000000LL, 0x0000000000000000LL }  /* pz */
     },
-    0x46f3234475d5add9LL, /* n */
-    0x035bdd1aeafdb0a9LL, /* nq */
+    0x0LL, /* n */
+    0x0LL, /* nq */
     "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd97"
   };
 
@@ -126,8 +126,8 @@ void test_ak_wcurve_discriminant_is_ok() {
     { 0x57cb446240dd1710LL, 0x7556091c4805caa4LL, 0xd0593365f9384bcdLL, 0x0fb1fbc48b0f0eb4LL }, /* r2q */
     {
       { 0x8b2582fe742daa28LL, 0x658b9196932e02c7LL, 0x880923425712b2bbLL, 0x91e38443a5e82c0dLL }, /* px */
-      { 0xaf268adb32322e5cLL, 0x5fde0b5344766740LL, 0x895786c4bb46e956LL, 0x32879423ab1a0375LL }, /* py */
-      { 0x0000000000000001LL, 0x0000000000000000LL, 0x0000000000000000LL, 0x0000000000000000LL }  /* pz */
+      { 0x0000000000000000LL }, /* py */
+      { 0x0000000000000000LL }  /* pz */
     },
     0x46f3234475d5add9LL, /* n */
     0x035bdd1aeafdb0a9LL, /* nq */
