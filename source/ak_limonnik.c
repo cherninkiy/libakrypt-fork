efdf24bc72dcb9da4b99ebc2417a6f5869f276c3
Berezin Alexander <aeberezin@edu.hse.ru>
2023-12-21 12:30:40 +0300

    Протестирован алгоритм, выполнены оптимизации

--- source/ak_limonnik.c
+++ source/ak_limonnik.c
@@ -5,8 +5,8 @@ static ak_uint64 h2[8] = {0x7e5534fa80104109, 0x7e6634fa80104109, 0x7e7734fa8010
 static ak_uint64 h3[8] = {0x7e6634fa80104109, 0x7e8834fa80104109, 0x7eaa34fa80104109, 0x7ecc34fa80104109, 0x7ebb34fa80104109, 0x7e9934fa80104109, 0x7e7734fa80104109, 0x7e5534fa80104109};
 
 int get_params_message_a(ak_wcurve E_b, ak_wpoint K_a, ak_pointer k_a);
-int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_a, ak_wcurve E_a, ak_wcurve E_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer s_b, ak_pointer tag_b, ak_bckey key, ak_pointer K_ab);
-int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer k_a, ak_pointer s_a, ak_pointer tag_b, ak_wcurve E_a, ak_wcurve E_b, ak_pointer tag_a, ak_pointer K_ba);
+int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_a, ak_wcurve E_a, ak_wcurve E_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer s_b, ak_pointer tag_b, ak_pointer K_ab);
+int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer k_a, ak_pointer s_a, ak_pointer tag_b, ak_wcurve E_a, ak_wcurve E_b, ak_pointer tag_a, ak_pointer K_ba, ak_bckey key);
 int auth_b(ak_pointer tag_a, ak_pointer Id_a, ak_pointer Id_b, ak_wpoint K_a, ak_wpoint K_b, ak_bckey key);
 
 
@@ -25,7 +25,7 @@ int get_params_message_a(ak_wcurve E_b, ak_wpoint K_a, ak_pointer k_a) {
     /* вызываем конструктор генератора */
     ak_random_create_hrng(&generator);
     /* вырабатываем случайное значение */
-    ak_mpzn_set_random_modulo(k_a, E_b->q, ak_mpzn512_size, &generator); // k_a = random(1, q_b-1)
+    ak_mpzn_set_random_modulo(k_a, E_b->q, ak_mpzn512_size, &generator);
     /* вызываем деструктор генератора */
     ak_random_destroy(&generator);
 
@@ -36,7 +36,7 @@ int get_params_message_a(ak_wcurve E_b, ak_wpoint K_a, ak_pointer k_a) {
 
 // 2 step
 
-int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_a, ak_wcurve E_a, ak_wcurve E_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer s_b, ak_pointer tag_b, ak_bckey key, ak_pointer K_ab) {
+int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_a, ak_wcurve E_a, ak_wcurve E_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer s_b, ak_pointer tag_b, ak_pointer K_ab) {
     /*
     Выработка случайного значения K_b = [k_b]P и tag_b
     Id_a - идентификатор стороны A
@@ -48,7 +48,6 @@ int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     K_b - точка K_a, в которую помещается результат нахождения случайной кратной точки [k_b]P
     s_b - секретный ключ стороны B
     tag_b - указатель на ak_uint64 [8] размера 512 бит, в который помещается результат нахождения tag_b
-    key - указатель на bckey, в который помещается ключ для вычисления MAC
     K_ab - указатель на ak_uint8 [32] размера 256 бит, в который помещается созданный ключ стороны B
     */
 
@@ -73,7 +72,7 @@ int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     /* вызываем конструктор генератора */
     ak_random_create_hrng(&generator);
     /* вырабатываем случайное значение */
-    ak_mpzn_set_random_modulo(k_b, E_a->q, ak_mpzn512_size, &generator); // k_b = random(1, q_a-1)
+    ak_mpzn_set_random_modulo(k_b, E_a->q, ak_mpzn512_size, &generator);
     /* вызываем деструктор генератора */
     ak_random_destroy(&generator);
 
@@ -84,12 +83,14 @@ int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     ak_wpoint_pow(&Q_ab, &Q_ab, (ak_uint64*)&E_a->cofactor, ak_mpzn512_size, E_a); // Q_ab = el_point((m_a/q_a)*k_b*S_a)
     ak_wpoint_pow(&R_ab, K_a, s_b, ak_mpzn512_size, E_b);
     ak_wpoint_pow(&R_ab, &R_ab, (ak_uint64*)&E_b->cofactor, ak_mpzn512_size, E_b); // R_ab = el_point((m_b/q_b)*s_b*K_a)
-    
+    ak_wpoint_reduce(&Q_ab, E_a);
+    ak_wpoint_reduce(&R_ab, E_b);
+
     struct hash h;
     ak_hash_create_streebog512(&h);
     ak_uint64 T_ab[ak_hash_get_tag_size(&h)];
     char in[64*4];
-    ak_snprintf(in, 64*4, "%d%d%d%d", Q_ab.x, R_ab.x, Id_a, Id_b);
+    ak_snprintf(in, 64*4, "%s%s%s%s", ak_mpzn_to_hexstr(Q_ab.x, 8), ak_mpzn_to_hexstr(R_ab.x, 8), ak_mpzn_to_hexstr(Id_a, 8), ak_mpzn_to_hexstr(Id_b, 8));
     ak_hash_finalize(&h, in, 64*4, T_ab, ak_hash_get_tag_size(&h)); // T_ab = KDF(Pi_func(Q_ab)||Pi_func(R_ab)||Id_a||Id_b)
     ak_hash_destroy(&h);
     
@@ -97,13 +98,13 @@ int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     memcpy(K_ab, T_ab, 32);
     memcpy(M_ab, T_ab+4, 32);
 
-    ak_bckey_create_kuznechik(key);
-    ak_bckey_set_key(key, M_ab, sizeof(M_ab));
+    struct bckey key;
+    ak_bckey_create_kuznechik(&key);
+    ak_bckey_set_key(&key, M_ab, sizeof(M_ab));
 
     char in2[64*5];
-    ak_snprintf(in2, 64*5, "%d%d%d%d%d", *h2, K_b->x, K_a->x, Id_b, Id_a);
-    ak_bckey_cmac(key, in2, 64*5, tag_b, 64); // tag_b = MAC_M_ab(h2||Pi_func(K_b)||Pi_func(K_a)||Id_b||Id_a)
-    ak_bckey_destroy(key);
+    ak_snprintf(in2, 64*5, "%s%s%s%s%s", ak_mpzn_to_hexstr(h2, 8), ak_mpzn_to_hexstr(K_b->x, 8), ak_mpzn_to_hexstr(K_a->x, 8), ak_mpzn_to_hexstr(Id_b, 8), ak_mpzn_to_hexstr(Id_a, 8));
+    ak_bckey_cmac(&key, in2, 64*5, tag_b, 64); // tag_b = MAC_M_ab(h2||Pi_func(K_b)||Pi_func(K_a)||Id_b||Id_a)
 
     memset(M_ab, 0, 32);
 
@@ -112,7 +113,7 @@ int check_params_from_message_b(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
 
 // 3 step
 
-int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer k_a, ak_pointer s_a, ak_pointer tag_b, ak_wcurve E_a, ak_wcurve E_b, ak_pointer tag_a, ak_pointer K_ba) {
+int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate Cert_b, ak_wpoint K_a, ak_wpoint K_b, ak_pointer k_a, ak_pointer s_a, ak_pointer tag_b, ak_wcurve E_a, ak_wcurve E_b, ak_pointer tag_a, ak_pointer K_ba, ak_bckey key) {
     /*
     Проверка полученного значения tag_b и выработка значения tag_a
     Id_a - идентификатор стороны A
@@ -127,6 +128,7 @@ int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     E_b - кривая E_b стороны В
     tag_a - указатель на ak_uint64 [8] размера 512 бит, в который помещается результат нахождения tag_a
     K_ba - указатель на ak_uint8 [32] размера 256 бит, в который помещается созданный ключ стороны А
+    key - указатель на bckey, в который помещается ключ для вычисления MAC
     */
 
     if (!ak_wpoint_is_ok(K_b, E_a)) { // check_point_in_curve(K_b, E_a)
@@ -149,13 +151,14 @@ int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     ak_wpoint_pow(&Q_ba, &Q_ba, (ak_uint64*)&E_a->cofactor, ak_mpzn512_size, E_a); // Q_ba = el_point((m_a/q_a)*s_a*K_b)
     ak_wpoint_pow(&R_ba, &S_b, k_a, ak_mpzn512_size, E_b);
     ak_wpoint_pow(&R_ba, &R_ba, (ak_uint64*)&E_b->cofactor, ak_mpzn512_size, E_b); // R_ba = el_point((m_b/q_b)*k_a*S_b)
-
+    ak_wpoint_reduce(&Q_ba, E_a);
+    ak_wpoint_reduce(&R_ba, E_b);
 
     struct hash h;
     ak_hash_create_streebog512(&h);
     ak_uint64 T_ba[ak_hash_get_tag_size(&h)];
     char in[64*4];
-    ak_snprintf(in, 64*4, "%d%d%d%d", Q_ba.x, R_ba.x, Id_a, Id_b);
+    ak_snprintf(in, 64*4, "%s%s%s%s", ak_mpzn_to_hexstr(Q_ba.x, 8), ak_mpzn_to_hexstr(R_ba.x, 8), ak_mpzn_to_hexstr(Id_a, 8), ak_mpzn_to_hexstr(Id_b, 8));
     ak_hash_finalize(&h, in, 64*4, T_ba, ak_hash_get_tag_size(&h)); // T_ba = KDF(Pi_func(Q_ba)||Pi_func(R_ba)||Id_a||Id_b)
     ak_hash_destroy(&h);
     
@@ -163,22 +166,21 @@ int check_params_from_message_a(ak_pointer Id_a, ak_pointer Id_b, ak_certificate
     memcpy(K_ba, T_ba, 32);
     memcpy(M_ba, T_ba+4, 32);
 
-    struct bckey key;
-    ak_bckey_create_kuznechik(&key);
-    ak_bckey_set_key(&key, M_ba, sizeof(M_ba));
+    ak_bckey_create_kuznechik(key);
+    ak_bckey_set_key(key, M_ba, sizeof(M_ba));
 
     char in2[64*5];
     ak_uint64 tag_b_[8];
-    ak_snprintf(in2, 64*5, "%d%d%d%d%d", *h2, K_b->x, K_a->x, Id_b, Id_a);
-    ak_bckey_cmac(&key, in2, 64*5, tag_b_, 64); // tag_b_ = MAC_M_ba(h2||Pi_func(K_b)||Pi_func(K_a)||Id_b||Id_a)
+    ak_snprintf(in2, 64*5, "%s%s%s%s%s", ak_mpzn_to_hexstr(h2, 8), ak_mpzn_to_hexstr(K_b->x, 8), ak_mpzn_to_hexstr(K_a->x, 8), ak_mpzn_to_hexstr(Id_b, 8), ak_mpzn_to_hexstr(Id_a, 8));
+    ak_bckey_cmac(key, in2, 64*5, tag_b_, 64); // tag_b_ = MAC_M_ba(h2||Pi_func(K_b)||Pi_func(K_a)||Id_b||Id_a)
 
     if (!ak_ptr_is_equal(tag_b, tag_b_, 8)) {
         printf("tag_b is not valid");
         return EXIT_FAILURE;
     } else {
         memset(in2, 0, 64*5);
-        ak_snprintf(in2, 64*5, "%d%d%d%d%d", *h3, K_a->x, K_b->x, Id_a, Id_b);
-        ak_bckey_cmac(&key, in2, 64*5, tag_a, 64); // tag_a = MAC_M_ba(h3||Pi_func(K_a)||Pi_func(K_b)||Id_a||Id_b)
+        ak_snprintf(in2, 64*5, "%s%s%s%s%s", ak_mpzn_to_hexstr(h3, 8), ak_mpzn_to_hexstr(K_a->x, 8), ak_mpzn_to_hexstr(K_b->x, 8), ak_mpzn_to_hexstr(Id_a, 8), ak_mpzn_to_hexstr(Id_b, 8));
+        ak_bckey_cmac(key, in2, 64*5, tag_a, 64); // tag_a = MAC_M_ba(h3||Pi_func(K_a)||Pi_func(K_b)||Id_a||Id_b)
 
         memset(M_ba, 0, 32);
 
@@ -200,12 +202,12 @@ int auth_b(ak_pointer tag_a, ak_pointer Id_a, ak_pointer Id_b, ak_wpoint K_a, ak
     */
     char in[64*5];
     ak_uint64 tag_a_[8];
-    ak_snprintf(in, 64*5, "%d%d%d%d%d", *h3, K_a->x, K_b->x, Id_a, Id_b);
-    ak_bckey_cmac(key, in, 64*5, tag_a, 64); // tag_a_ = MAC_M_ab(h3||Pi_func(K_a)||Pi_func(K_b)||Id_a||Id_b)
+    ak_snprintf(in, 64*5, "%s%s%s%s%s", ak_mpzn_to_hexstr(h3, 8), ak_mpzn_to_hexstr(K_a->x, 8), ak_mpzn_to_hexstr(K_b->x, 8), ak_mpzn_to_hexstr(Id_a, 8), ak_mpzn_to_hexstr(Id_b, 8));
+    ak_bckey_cmac(key, in, 64*5, tag_a_, 64); // tag_a_ = MAC_M_ab(h3||Pi_func(K_a)||Pi_func(K_b)||Id_a||Id_b)
     ak_bckey_destroy(key);
 
     if (!ak_ptr_is_equal(tag_a, tag_a_, 64)) {
-        printf("tag_a is bot valid");
+        printf("tag_a is not valid");
         return EXIT_FAILURE;
     } else {
         return EXIT_SUCCESS;
@@ -224,7 +226,7 @@ int main() {
     ak_uint64 Id_b[8] = {0x7e6634fa80104109, 0x7e8834fa80104109, 0x7eaa34fa80104109, 0x7ecc34fa80104109, 0x7ebb34fa80104109, 0x7e9934fa80104109, 0x7e7734fa80104109, 0x7e5534fa80104109};
 
     struct wcurve E_a = id_tc26_gost_3410_2012_512_paramSetA;
-    struct wcurve E_b = id_tc26_gost_3410_2012_512_paramSetA;
+    struct wcurve E_b = id_tc26_gost_3410_2012_512_paramSetB;
 
     ak_uint64 s_a[ak_mpzn512_size], s_b[ak_mpzn512_size];
     /* определяем контекст генератора псевдослучайных значений */
@@ -256,8 +258,8 @@ int main() {
     ak_uint8 K_ab[32], K_ba[32];
 
     if (get_params_message_a(&E_b, &K_a, k_a) == EXIT_SUCCESS) {
-        if (check_params_from_message_b(Id_a, Id_b, &Cert_a, &E_a, &E_b, &K_a, &K_b, s_b, tag_b, &key, K_ab) == EXIT_SUCCESS) {
-            if (check_params_from_message_a(Id_a, Id_b, &Cert_b, &K_a, &K_b, k_a, s_a, tag_b, &E_a, &E_b, tag_a, K_ba) == EXIT_SUCCESS) {
+        if (check_params_from_message_b(Id_a, Id_b, &Cert_a, &E_a, &E_b, &K_a, &K_b, s_b, tag_b, K_ab) == EXIT_SUCCESS) {
+            if (check_params_from_message_a(Id_a, Id_b, &Cert_b, &K_a, &K_b, k_a, s_a, tag_b, &E_a, &E_b, tag_a, K_ba, &key) == EXIT_SUCCESS) {
                 if (auth_b(tag_a, Id_a, Id_b, &K_a, &K_b, &key) == EXIT_SUCCESS) {
                     printf("%s\n", ak_ptr_to_hexstr(K_ab, 32, 0));
                     printf("%s\n", ak_ptr_to_hexstr(K_ba, 32, 0));
