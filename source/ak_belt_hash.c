94d999504037da50ebd3e591024091b1cf2689bd
Timothy Tur <tvtur@edu.hse.ru>
2025-02-05 15:20:56 +0300

    making strange test

--- source/ak_belt_hash.c
+++ source/ak_belt_hash.c
@@ -328,7 +328,7 @@ static int ak_hash_context_belt_hash_clean( ak_pointer bctx ) {
   printf("\n1: %s\n   %i\n   %s\n",
     ak_ptr_to_hexstr(cx->h, sizeof(cx->h), ak_false),
     size,
-    ak_ptr_to_hexstr(in, size, ak_false));
+    ak_ptr_to_hexstr(in, size+16, ak_false));
 
   // обновить длину
   carry = (cx->ls[0] += carry) < carry;
