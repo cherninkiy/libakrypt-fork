6230b157388c45533939f20f3c90a8162b2e7201
Timothy Tur <tvtur@edu.hse.ru>
2025-02-05 19:17:13 +0300

    like for real

--- source/ak_belt_hash.c
+++ source/ak_belt_hash.c
@@ -390,20 +390,20 @@ static int ak_hash_context_belt_hash_finalize( ak_pointer bctx,
   carry = (ak_uint32*) &count_for_r;
 
   printf("1: %i %li %i %i\n",
-    cx->ls[0], count_for_r, carry[0], carry[1]);
+    bx->ls[0], count_for_r, carry[0], carry[1]);
 
-  carry[0] = (cx->ls[0] += carry[0]) < carry[0];
-  carry[0] = (cx->ls[1] += carry[0]) < carry[0];
-  carry[0] = (cx->ls[2] += carry[0]) < carry[0];
-  cx->ls[3] += carry[0];
+  carry[0] = (bx->ls[0] += carry[0]) < carry[0];
+  carry[0] = (bx->ls[1] += carry[0]) < carry[0];
+  carry[0] = (bx->ls[2] += carry[0]) < carry[0];
+  bx->ls[3] += carry[0];
 
-  carry[1] = (cx->ls[1] += carry[1]) < carry[1];
-  carry[1] = (cx->ls[2] += carry[1]) < carry[1];
-  cx->ls[3] += carry[1];
+  carry[1] = (bx->ls[1] += carry[1]) < carry[1];
+  carry[1] = (bx->ls[2] += carry[1]) < carry[1];
+  bx->ls[3] += carry[1];
 
   count_for_r = size >> 61;
-  carry[0] = (cx->ls[2] += carry[0]) < carry[0];
-  cx->ls[3] += carry[0];
+  carry[0] = (bx->ls[2] += carry[0]) < carry[0];
+  bx->ls[3] += carry[0];
 
   /*
   carry[0] = (cx->ls[0] += carry[0]) < carry[0];
