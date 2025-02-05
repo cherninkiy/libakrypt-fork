aafa71df92f54fb3b8c69c87732d894dfcb1e6f3
Timothy Tur <tvtur@edu.hse.ru>
2025-02-05 15:29:08 +0300

    making strange test x4

--- source/ak_belt_hash.c
+++ source/ak_belt_hash.c
@@ -325,13 +325,14 @@ static int ak_hash_context_belt_hash_clean( ak_pointer bctx ) {
   if( size & 0x1F ) return ak_error_message( ak_error_wrong_length, __func__,
                                       "data length is not a multiple of the length of the block" );
 
-  count += 16
+  count += 16;
   printf("\n1: %s\n   %i\n",
     ak_ptr_to_hexstr(cx->h, sizeof(cx->h), ak_false),
     size);
   printf("   %i\n   %s\n",
     count,
     ak_ptr_to_hexstr(in, count, ak_false));
+  count -= 16;
 
   // обновить длину
   carry = (cx->ls[0] += carry) < carry;
